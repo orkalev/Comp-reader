a38d6ecabe0cf87cd8127946c9cfa9bf7b9e6f2f
Or Kalev <orkalev@gmail.com>
2020-11-14 16:28:42 +0200

    readME file

--- reader.ml
+++ reader.ml
@@ -29,7 +29,8 @@ let rec sexpr_eq s1 s2 =
   | Pair(car1, cdr1), Pair(car2, cdr2) -> (sexpr_eq car1 car2) && (sexpr_eq cdr1 cdr2)
   | _ -> false;;
 
-module Reader: sig
+module Reader
+: sig
   val read_sexprs : string -> sexpr list
 end
 = struct
@@ -42,5 +43,13 @@ let normalize_scheme_symbol str =
 
 
 let read_sexprs string = raise X_not_yet_implemented;;
-  
 end;; (* struct Reader *)
+
+let make_paired nt_left nt_right nt =
+  let nt = caten nt_left nt in
+  let nt = pack nt (fun (_,d) -> d) in
+  let nt = caten nt nt_right in
+  pack nt (fun (d,_) -> d);;
+
+(*nt_whitespace*)
+let nt_whitespace = star (char ' ');;
---
+++ sample.ml
@@ -0,0 +1,12 @@
+#use "reader.ml";;
+open Reader;;
+
+#use "topfind";;
+#use "pc.ml";;
+open PC;;
+
+
+(* let test_tag_raises_6 test_ctxt = assert_raises X_this_should_not_happen (fun _ -> (Reader.read_sexpr "(1 #{foo}=2 3 #{foo}=4)"));;
+let test_tag_raises_7 test_ctxt = assert_raises X_this_should_not_happen (fun _ -> (Reader.read_sexpr "(1 (#{x}=5 6) 7 8 (#{x}=3 5) 9)"));; *)
+
+Reader.read_sexprs "(1 (#{x}=5 6) 7 8 (#{x}=3 5) 9)"
